31f452c637efa568a0c205ece7d4af6a69c3590b
Hsiao CH <gitgithubhub0000@gmail.com>
2021-08-05 11:50:24 +0800

    update version 0.0.2
    update version 0.0.2
    
    update version 0.02

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Caculator
 
+> version 0.0.2 
+>> Quadratic operations on Numbers
+>
 > version 0.0.1
 >> Quadratic operations on integers(no brackets)
 >
-> version 0.0.2 (not yet)
->> Quadratic operations on integers
--- caculator.js
+++ caculator.js
@@ -9,11 +9,21 @@ function getp(op){
     }
 }
 function infixToPofix(infix){
-    let infixArr=infix.split(/\s*/);
+    let infixArr=infix.split(/\s+/);
     let operatorStack=[];
     let output=[];
     for(let op of infixArr){
-        if(getp(op)>0){
+        if(op==='('){
+            operatorStack.push(op);
+        }
+        else if(op===')'){
+            while(true){
+                let p=operatorStack.pop();
+                if(p==='(')break;
+                output.push(p);
+            }
+        }
+        else if(getp(op)>0){
             while(operatorStack.length>0){
                       let topop=operatorStack.pop();
                       if(getp(op)<=getp(topop)){
@@ -39,7 +49,11 @@ function opcal(st,operator){
 function postfixCal(pofix){
     let tempStack=[];
     for(let op of pofix){
+<<<<<<< HEAD
         if(getp(op)>0) tempStack.push(opcal(tempStack,op))
+=======
+        if(getp(op)>0)tempStack.push(opcal(tempStack,op))
+>>>>>>> b2cc120 (some condition optimization)
         else tempStack.push(Number(op));
     }
     return tempStack.pop();
